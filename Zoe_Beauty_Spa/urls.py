8e509a27604ec5b390e64ac5b1b4bbfa8368d8fc
jose260589 <jose260589@yahoo.com>
2021-12-08 21:24:31 -0400

    quedo pienden archivos de commit anterior, archivos urls2 y home.html

--- Zoe_Beauty_Spa/urls.py
+++ Zoe_Beauty_Spa/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from GestionSPA import views
 #from Zoe_Beauty_Spa.GestionSPA.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="Home"),
-    path('servicios/', views.servicios, name="Servicios"),
-    path('tienda/', views.tienda, name="Tienda"),
-    path('blog/', views.blog, name="Blog"),
-    path('contacto/', views.contacto, name="Contacto"),
+
 
 ]
