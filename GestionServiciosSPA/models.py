952d20866e97b1356f0a9d3ac53750ece0090dfc
jose260589 <jose260589@yahoo.com>
2021-12-15 10:56:15 -0400

    se agrego costo en model, se realizaron dos makemigrations, pero solo se ejecuto la 0003, la 0002, tenia un defecto.

--- GestionServiciosSPA/models.py
+++ GestionServiciosSPA/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from django.db import models as m
 from django.db.models.fields.related import create_many_to_many_intermediary_model
 
@@ -7,6 +8,7 @@ class Servicio(m.Model):
     titulo=m.CharField(max_length=50)
     contenido=m.CharField(max_length=100)
     imagen=m.ImageField()
+    costo=m.IntegerField()
     created=m.DateTimeField(auto_now_add=True)
     updated=m.DateTimeField(auto_now_add=True)
 
