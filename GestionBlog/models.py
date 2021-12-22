602e81790809fbbce21dea68b7bc816575c48e72
jose260589 <jose260589@yahoo.com>
2021-12-22 14:11:19 -0400

    se agregan datos a archivo models de GestionBlog para construir datos tablas, categoria y post, se modifica archivo urls de aplicacion principal

--- GestionBlog/models.py
+++ GestionBlog/models.py
@@ -1,6 +1,33 @@
 from django.db import models as m
+from django.contrib.auth.models import User
+
+#from Zoe_Beauty_Spa.GestionServiciosSPA import models
 
 # Create your models here.
 
 class Categoria(m.Model):
-    pass
+    nombre=m.CharField(max_length=50)
+    created=m.DateTimeField(auto_now_add=True)
+    updated=m.DateTimeField(auto_now_add=True)
+
+    class Meta():
+        verbose_name='categoria'
+        verbose_name_plural='categorias'
+
+    def __str__(self):
+        return self.nombre
+
+class Post(m.Model):
+    titulo=m.CharField(max_length=50)
+    contenido=m.CharField(max_length=100)
+    imagen=m.ImageField(upload_to='blog', null=True, blank=True) #con este metodo se puede cargar las fotos a la carpeta en media
+    autor=m.ForeignKey(User, on_delete=m.CASCADE)
+    created=m.DateTimeField(auto_now_add=True)
+    updated=m.DateTimeField(auto_now_add=True)
+    categorias=m.ManyToManyField(Categoria)
+    class Meta():
+        verbose_name='post'
+        verbose_name_plural='posts'
+
+    def __str__(self):
+        return self.titulo
--- Zoe_Beauty_Spa/urls.py
+++ Zoe_Beauty_Spa/urls.py
@@ -19,8 +19,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('GestionSPA.urls')),
     path('', include('GestionServiciosSPA.urls')),
+    path('', include('GestionBlog.urls')),
 ]
+
