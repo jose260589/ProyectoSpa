8f2308cdc90ce3abe1f8876c17ddd474011e6573
jose260589 <jose260589@yahoo.com>
2021-12-22 13:30:48 -0400

    se modifico archivo urls.py para que pueda gestionar imagenes y tambien los views.py

---
+++ GestionBlog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from GestionBlog import views
+
+#configuracion para media
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+
+   # path('servicios/', views.servicios, name="Servicios"),
+
+]
+
+#Configuracion acceso a direcion carpeta media
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
