02f01d469d4b3bf5c0276b3f77e27a78ca368768
jose260589 <jose260589@yahoo.com>
2021-12-23 09:58:01 -0400

    se modifico templates y views.py para verificar error de que no aparecen los blog en la pagina, solucionado

--- GestionBlog/views.py
+++ GestionBlog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from GestionBlog.models import Post
 
 # Create your views here.
 def blog(request):
-
-    return render(request, "blog.html")
+    posts=Post.objects.all()
+    return render(request, "blog.html", {"posts": posts}) 
+    """{"posts": posts} deben ser iguales, de lo contrario no funciona"""
