f161771185ce86c6f8c956d3bb5094d6ff30d6a5
jose260589 <jose260589@yahoo.com>
2022-01-02 21:09:54 -0400

    se creo formulario en contacto html. se modifico error en views.py de gestioncontacto, el cual se cargo mal

--- GestionContacto/forms.py
+++ GestionContacto/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class Formulariocontacto(forms.Form):
-    nombre=forms.CharField(label="Nombre", required=True)
-    email=forms.CharField(label="Email", required=True)
-    contenido=forms.CharField(label="Nombre")
+    nombre=forms.CharField(label="Nombre", required=True, max_length=50)
+    email=forms.CharField(label="Email", required=True, max_length=50)
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)
--- GestionContacto/views.py
+++ GestionContacto/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render,HttpResponse
-from django.forms import formu
+#from Zoe_Beauty_Spa.GestionContacto.forms import Formulariocontacto
 
-from forms import FormularioconFormulariocontacto
+from .forms import Formulariocontacto
 
 # Create your views here.
 def contacto(request):
-
-    return render(request, "contacto.html")
+    formulario_contacto=Formulariocontacto()
+    return render(request, "contacto.html", {'miFormulario':formulario_contacto})
